bde51569d8dbbd6d0b681c61a71d1e462eff5632
yumaito-mac <yumaito0@gmail.com>
2016-10-22 17:28:29 +0900

    adjust indent

--- lib/musical_score/const.rb
+++ lib/musical_score/const.rb
@@ -1,10 +1,12 @@
 module MusicalScore
     SHARP = :sharp
     FLAT  = :flat
-    NUMBER_OF_NOTES  = 12
-    NUMBER_OF_FIFTHS = 7
+
+    NUMBER_OF_NOTES   = 12
+    NUMBER_OF_FIFTHS  = 7
     SHARP_START_INDEX = 11
     FLAT_START_INDEX  = 6
+
     RELATED_KEY_SLIDE_NUMBER = 9
 
     AVAILABLE_NUMBERS_OF_ALTER = [-2, -1, 0, 1, 2]
