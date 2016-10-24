690aacbcd3e8c19b352b47f6a63c12b01627ab7f
yumaito-mac <yumaito0@gmail.com>
2016-10-25 08:06:15 +0900

    mod attribute test

--- musical_score.gemspec
+++ musical_score.gemspec
@@ -33,7 +33,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "rspec", "~> 3.1"
   spec.add_development_dependency "fuubar"
   spec.add_development_dependency "yard"
   spec.add_development_dependency "yard-contracts"
--- spec/attribute/key_spec.rb
+++ spec/attribute/key_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe MusicalScore::Attribute::Key do
     describe 'initialize' do
-        let(:key) { MusicalScore::Attribute::Key.new(3, :major) }
         it 'create key object' do
-            expect(key.fifths).to eq 3
-            expect(key.mode).to eq :major
+            key = MusicalScore::Attribute::Key.new(3, :major)
+            expect(key).to have_attributes(fifths: 3, mode: :major)
         end
 
         it 'raise ArgumentError' do
@@ -17,17 +16,17 @@ describe MusicalScore::Attribute::Key do
     end
 
     describe 'altered_pitches' do
-        let(:c_major) { MusicalScore::Attribute::Key.new(0, :major)}
-        let(:altered_pitches) {c_major.tonic_key_and_altered_pitches}
         it 'C major' do
+            c_major = MusicalScore::Attribute::Key.new(0, :major)
+            altered_pitches = c_major.tonic_key_and_altered_pitches
             expect(altered_pitches[:major_pitch]).to eq MusicalScore::Note::Pitch.new(:C)
             expect(altered_pitches[:minor_pitch]).to eq MusicalScore::Note::Pitch.new(:A)
             expect(altered_pitches[:altered_pitches].size).to eq 0
         end
 
-        let(:e_major) { MusicalScore::Attribute::Key.new(4, :major)}
-        let(:altered_pitches_e) {e_major.tonic_key_and_altered_pitches}
         it 'E major' do
+            e_major = MusicalScore::Attribute::Key.new(4, :major)
+            altered_pitches_e = e_major.tonic_key_and_altered_pitches
             expect(altered_pitches_e[:major_pitch]).to eq MusicalScore::Note::Pitch.new(:E)
             expect(altered_pitches_e[:minor_pitch]).to eq MusicalScore::Note::Pitch.new(:C, 1)
             expect(altered_pitches_e[:altered_pitches]).to match([
@@ -38,9 +37,9 @@ describe MusicalScore::Attribute::Key do
             ])
         end
 
-        let(:c_sharp_major) { MusicalScore::Attribute::Key.new(7, :major)}
-        let(:altered_pitches_cs) { c_sharp_major.tonic_key_and_altered_pitches }
         it 'C sharp major' do
+            c_sharp_major = MusicalScore::Attribute::Key.new(7, :major)
+            altered_pitches_cs = c_sharp_major.tonic_key_and_altered_pitches
             expect(altered_pitches_cs[:major_pitch]).to eq MusicalScore::Note::Pitch.new(:C, 1)
             expect(altered_pitches_cs[:minor_pitch]).to eq MusicalScore::Note::Pitch.new(:A, 1)
             expect(altered_pitches_cs[:altered_pitches]).to match([
@@ -54,9 +53,9 @@ describe MusicalScore::Attribute::Key do
             ])
         end
 
-        let(:e_flat_major) { MusicalScore::Attribute::Key.new(-3, :major)}
-        let(:altered_pitches_ef) { e_flat_major.tonic_key_and_altered_pitches }
         it 'E flat major' do
+            e_flat_major = MusicalScore::Attribute::Key.new(-3, :major)
+            altered_pitches_ef = e_flat_major.tonic_key_and_altered_pitches
             expect(altered_pitches_ef[:major_pitch]).to eq MusicalScore::Note::Pitch.new(:E, -1)
             expect(altered_pitches_ef[:minor_pitch]).to eq MusicalScore::Note::Pitch.new(:C)
             expect(altered_pitches_ef[:altered_pitches]).to match([
@@ -66,9 +65,9 @@ describe MusicalScore::Attribute::Key do
             ])
         end
 
-        let(:g_flat_major) { MusicalScore::Attribute::Key.new(-6, :major)}
-        let(:altered_pitches_gf) { g_flat_major.tonic_key_and_altered_pitches }
         it 'G flat major' do
+            g_flat_major = MusicalScore::Attribute::Key.new(-6, :major)
+            altered_pitches_gf = g_flat_major.tonic_key_and_altered_pitches
             expect(altered_pitches_gf[:major_pitch]).to eq MusicalScore::Note::Pitch.new(:G, -1)
             expect(altered_pitches_gf[:minor_pitch]).to eq MusicalScore::Note::Pitch.new(:E, -1)
             expect(altered_pitches_gf[:altered_pitches]).to match([
