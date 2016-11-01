bde5eb1be3101d3591d73b6f414bb885a9ddf5c5
yumaito-mac <yumaito0@gmail.com>
2016-11-01 21:37:17 +0900

    add import method

--- lib/musical_score/io/importer.rb
+++ lib/musical_score/io/importer.rb
@@ -2,8 +2,19 @@ require 'musical_score'
 require 'rexml/document'
 module MusicalScore
     module IO
+        def import(file)
+            extname = File.extname(file)
+            case extname
+            when ".xml"
+                return import_xml(file)
+            else
+                raise MusicalScore::InvalidFileType
+            end
+        end
         def import_xml(file_path)
             xml = REXML::Document.new(File.new(file_path))
         end
+
+        module_function :import
     end
 end
---
+++ spec/io/importer_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe MusicalScore::IO do
+    describe 'import' do
+        it do
+            allow(MusicalScore::IO).to receive(:import_xml)
+            MusicalScore::IO.import("hoge.xml")
+            expect(MusicalScore::IO).to have_received(:import_xml).once
+        end
+
+        it do
+            expect{ MusicalScore::IO.import("hoge.jpg") }.to raise_error(MusicalScore::InvalidFileType)
+        end
+    end
+end
