e5e984b9c103dfe60cc88e934dd2c0b5c5984088
yumaito-mac <yumaito0@gmail.com>
2016-11-03 23:46:47 +0900

    add identification test

--- spec/score/identification/identification_spec.rb
+++ spec/score/identification/identification_spec.rb
@@ -13,4 +13,42 @@ describe MusicalScore::Score::Identification do
             expect(identification.encoding.encoding_description).to eq "hogehoge"
         end
     end
+
+    describe 'create_by_xml' do
+        let(:dummy_identification_xml) {
+            '<identification>
+    <creator type="composer">Ludwig van Beethoven</creator>
+    <encoding>
+      <software>Finale 2012 for Windows</software>
+      <software>Dolet Light for Finale 2012</software>
+      <encoding-date>2012-09-06</encoding-date>
+      <supports attribute="new-system" element="print" type="yes" value="yes"/>
+      <supports attribute="new-page" element="print" type="yes" value="yes"/>
+    </encoding>
+  </identification>'
+        }
+        let(:no_creator) {
+            '<identification>
+    <encoding>
+      <software>Finale 2012 for Windows</software>
+      <software>Dolet Light for Finale 2012</software>
+      <encoding-date>2012-09-06</encoding-date>
+      <supports attribute="new-system" element="print" type="yes" value="yes"/>
+      <supports attribute="new-page" element="print" type="yes" value="yes"/>
+    </encoding>
+  </identification>'
+        }
+        it do
+            xml = dummy_xml(dummy_identification_xml)
+            identification = MusicalScore::Score::Identification::Identification.create_by_xml(xml.elements["//identification"])
+            expect(identification.creators[0].name).to eq "Ludwig van Beethoven"
+            expect(identification.encoding.supports).to match(['print', 'print'])
+        end
+        it do
+            xml = dummy_xml(no_creator)
+            identification = MusicalScore::Score::Identification::Identification.create_by_xml(xml.elements["//identification"])
+            expect(identification.creators).to match([])
+            expect(identification.encoding.supports).to match(['print', 'print'])
+        end
+    end
 end
