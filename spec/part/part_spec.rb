7382ec192b0122ff41cbf0fada7e3cb5385bed80
yumaito-mac <yumaito0@gmail.com>
2016-11-13 00:30:21 +0900

    add export_xml of part-list test

--- spec/part/part_spec.rb
+++ spec/part/part_spec.rb
@@ -8,4 +8,18 @@ describe MusicalScore::Part::Part do
             expect(part).to have_attributes(measures: measures)
         end
     end
+
+    describe 'export_xml' do
+        let(:dummy_part_xml) {
+            '<score-part id="P1">
+    <part-name>Trumpet in Bb</part-name>
+    <part-abbreviation>Bb Tpt.</part-abbreviation>
+</score-part>'
+        }
+        it do
+            xml  = dummy_xml(dummy_part_xml)
+            part = MusicalScore::Score::Part::Part.new("Trumpet in Bb", "Bb Tpt.")
+            expect(format_xml(part.export_xml(1))).to eq format_xml(xml.elements["score-part"])
+        end
+    end
 end
