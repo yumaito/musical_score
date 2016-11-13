afa78ebf48c88724d5245e2bc2e171b95b479069
yumaito-mac <yumaito0@gmail.com>
2016-11-13 14:26:35 +0900

    add export part_list on export score method

--- lib/musical_score/score/score.rb
+++ lib/musical_score/score/score.rb
@@ -101,11 +101,11 @@ module MusicalScore
                     part_list_element.add_element(index + 1)
                 end
                 score_partwise.add_element(part_list_element)
-                # part_list = REXML::Element.new('part_list')
-                # @part_list.each_with_index do |part, index|
-                #     part_list.add_element(part.export_xml, index)
-                # end
-                # score_partwise.add_element(part_list)
+                part_list = REXML::Element.new('part_list')
+                @part_list.each_with_index do |part, index|
+                    part_list.add_element(part.export_xml(index))
+                end
+                score_partwise.add_element(part_list)
                 #
                 # @parts.each_with_index do |part, index|
                 #     score_partwise.add_element(part.export_xml, index)
