df85d5fb3fe1380f4fca1616d66664dd1edbc26d
yumaito-mac <yumaito0@gmail.com>
2016-11-20 15:46:34 +0900

    add create_by_hash on credits and score-part

--- lib/musical_score/score/score.rb
+++ lib/musical_score/score/score.rb
@@ -80,6 +80,22 @@ module MusicalScore
                     identification = MusicalScore::Score::Identification::Identification.create_by_hash(doc["identification"][0])
                     args[:identification] = identification
                 end
+
+                credits = Array.new
+                doc["credit"].each do |element|
+                    if (element.has_key?("credit-words"))
+                        credits.push(element.dig("credit-words", 0, "content"))
+                    end
+                end
+
+                part_list = Array.new
+                doc.dig("part-list", 0, "score-part").each do |element|
+                    part_name         = element["part-name"][0]
+                    part_abbreviation = element["part-abbreviation"][0]
+                    part = MusicalScore::Score::Part::Part.new(part_name, part_abbreviation)
+                    part_list.push(part)
+                end
+                args[:part_list] = part_list
             end
 
             def export_xml()
